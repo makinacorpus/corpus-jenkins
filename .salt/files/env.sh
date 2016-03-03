5f1c3f98d564954b68ddae363e3bed2d51b15c2f
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2016-03-03 15:57:38 +0100

    BUG: Missing '$' in bash, and salt forloop on the wrong place

--- .salt/files/env.sh
+++ .salt/files/env.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 {% set cfg = salt['mc_project.get_configuration'](cid) %}
-{% for i,val in cfg.data.env.items() %} 
 for i in /etc/default/jenkins "{{cfg.data_root}}/jenkins-default";do
-    if test -r "{i}";then
+    if test -r "${i}";then
         source "${i}"
     fi
 done
+{% for i,val in cfg.data.env.items() %}
 {{i}}="{{val}}"
 export {{i}}
 {% endfor %}
